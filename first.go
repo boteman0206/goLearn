53db877680c0834da7a3cb0b1f663847d56c8994
wei.peng <wei.peng@marcpoint.com>
2020-09-24 23:14:27 +0800

    初始化

---
+++ first.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt" // 导入 fmt 包，打印字符串是需要用到
+)
+
+func a() { // 声明 main 主函数
+	fmt.Println("this is the Hello World!") // 打印 Hello World!
+}
+
+func main() {
+	a()
+}
+
+//go run  path    直接运行， 不会生成可执行文件    速度慢
+
+// go build path   会进行编译成  .exe的可执行文件  速度快
+
+// go build进行编译的时候回直接打包运行环境， 在没有sdk的环境下面依然可以直接执行
