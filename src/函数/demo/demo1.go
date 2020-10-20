ac64293c85c867de674a5258c95e0d808a7e2d89
wei.peng <wei.peng@marcpoint.com>
2020-10-20 21:55:12 +0800

    函数的使用

---
+++ src/函数/demo/demo1.go
@@ -0,0 +1,5 @@
+package demo
+
+func Cal(a int, b int) int {
+	return a + b
+}
---
+++ src/函数/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"../demo"
+	"fmt"
+)
+
+func main() {
+
+	cal := demo.Cal(16, 18)
+	fmt.Println("cal : ", cal)
+
+}
