937fbcfe10acdf1707b697be54ad04f7084659f8
wei.peng <wei.peng@marcpoint.com>
2020-10-26 22:13:42 +0800

    闭包的使用

--- src/函数/闭包的使用/demo.go
+++ src/函数/闭包的使用/demo.go
@@ -1,1 +1,26 @@
-package 闭包的使用
+package main
+
+/**
+
+ */
+
+func main() {
+
+	one := nextOne()
+	fmt.Println(one())
+	fmt.Println(one())
+	fmt.Println(one())
+
+}
+
+/**
+返回的是匿名函数，但是匿名函数引用到函数外部的i1，因此这个匿名函数就和i1
+形成一个整体，就是闭包。i1并不会重新初始化
+*/
+func nextOne() func() int {
+	i1 := 3
+	return func() int {
+		i1 += 1
+		return i1
+	}
+}
