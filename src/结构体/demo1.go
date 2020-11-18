6f24b5c97b27f03cb64ff0ea7c34d5daf760f24d
wei.peng <wei.peng@marcpoint.com>
2020-11-18 20:35:13 +0800

    结构体的使用

--- src/结构体/demo1.go
+++ src/结构体/demo1.go
@@ -16,6 +16,8 @@ type Cat struct {
 }
 
 func main() {
+
+	//创建方式一
 	var cat Cat
 	cat.name = "小白"
 	cat.age = 89
@@ -33,5 +35,28 @@ func main() {
 	cat.slice1[0] = 9
 
 	fmt.Println("cat map ", cat)
+
+	//创建方式二 有多余的字段需要指定属性即可
+	s := Cat{name: "小黑", age: 12, color: "黑色"}
+	fmt.Println("s : ", s)
+
+	// 方式三 指针方式
+	var p3 *Cat = new(Cat)
+	fmt.Println("p3: ", *p3)
+	(*p3).name = "simth" // 标准写法
+	p3.age = 90          // 这是对上面取地址的简化，底层自动执行
+	fmt.Println("地址赋值： ", *p3)
+
+	// 方式四 &地址符号
+	var p2 *Cat = &Cat{}
+	(*p2).name = "lucy"
+	p2.age = 88
+	fmt.Println("p2 : ", p2)
+
+	var s2 Cat = s // 结构体是进行值拷贝的 ，修改s2的name不会影响s的值
+
+	s2.name = "修改s2"
+	fmt.Println("s1修改后 :", s)
+	fmt.Println("s2 :", s2)
 
 }
