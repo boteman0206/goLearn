6e58013a34195e5e885ef7b9c0813c400f4d28fa
wei.peng <wei.peng@marcpoint.com>
2020-11-19 20:35:33 +0800

    继承的注意事项

--- src/面向对象/demo.go
+++ src/面向对象/demo.go
@@ -53,7 +53,17 @@ func main() {
 	/*
 		1: 结构体可以使用匿名结构体中的所有字段和方法（包括大写和小写）
 		2：子类的字段会覆盖父类结构体中的字段，先去子类中找对应的字段（就近访问原则）
+		3： 如果继承了多个结构体，并且子类中没有指定的字段，必须指定某一个匿名结构体的字段属性，否则会报错
+		4：嵌套有名的接固体必须指定结构体的名称
+		type D struct {
+			a A
+		}
+		必须带上 d.a.name 才可以
 
 	*/
+
+	i := &student.Dog{student.Animols{"jack", 19}}
+	fmt.Println("I : ", i.Animols.Name)
+	fmt.Println("I : ", i.Name)
 
 }
