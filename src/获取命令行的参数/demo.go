0fdf37a4da2b3d1b09d6982443f687bf5bf3993d
wei.peng <wei.peng@marcpoint.com>
2020-12-03 22:02:54 +0800

    os.args接受参数

---
+++ src/获取命令行的参数/demo.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	// 接受参数的os.Args包
+	strings := os.Args
+	fmt.Println("main ===========run ")
+	fmt.Println("strings : ", strings)
+
+	/**
+
+
+	E:\goProject\goLearn\src\获取命令行的参数>go build demo.go
+	E:\goProject\goLearn\src\获取命令行的参数>demo.exe abc num
+	main ===========run
+	strings :  [demo.exe abc num]
+
+	*/
+
+	//使用flag包来接受参数
+	var user string
+	var pwd string
+
+	flag.StringVar(&user, "u", "", "获取用户名")
+	flag.StringVar(&pwd, "pwd", "默认值", "获取密码")
+
+	flag.Parse()
+
+	fmt.Println("user : ", user, " pwd: ", pwd)
+
+	/**
+
+		 E:\goProject\goLearn\src\获取命令行的参数>demo.exe -u "jack" -pwd "qeweq
+	e"
+		main ===========run
+		strings :  [demo.exe -u jack -pwd qeweqe]
+		user :  jack  pwd:  qeweqe
+
+	*/
+}
