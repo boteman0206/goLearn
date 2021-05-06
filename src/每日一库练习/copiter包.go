6ed956b0d84039d0aabe9746f420b6767a84f35a
csf <410934912@qq.com>
2021-05-06 18:29:20 +0800

    不同结构体之间的赋值copiter

--- src/每日一库练习/copiter包.go
+++ src/每日一库练习/copiter包.go
@@ -26,4 +26,23 @@ func main() {
 	user.Name = "pop"
 	fmt.Println("user : ", user)
 	fmt.Println(employee)
+
+	fmt.Println()
+	// todo 切片赋值
+	users := []User{
+		{Name: "dj", Age: 18},
+		{Name: "dj2", Age: 18},
+	}
+	employees := []Employee{}
+
+	copier.Copy(&employees, &users)
+	fmt.Printf("%#v\n", employees)
+
+	fmt.Println()
+	//todo 将结构体赋值到切片中
+	user1 := User{Name: "dj", Age: 18}
+	employees1 := []Employee{}
+
+	copier.Copy(&employees1, &user1)
+	fmt.Printf("%#v\n", employees1)
 }
