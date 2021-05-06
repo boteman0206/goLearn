90b70650a4a3a7486c7c06c12e3ff73f26ebdb2a
csf <410934912@qq.com>
2021-05-06 18:14:54 +0800

    不同结构体之间的赋值copiter

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/imdario/mergo v0.3.12
+	github.com/jinzhu/copier v0.3.0
 	github.com/jmoiron/sqlx v1.3.3
 	github.com/json-iterator/go v1.1.11
 	github.com/spf13/cast v1.3.1
---
+++ src/每日一库练习/copiter包.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/copier"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type Employee struct {
+	Name string
+	Age  int
+	Role string
+}
+
+func main() {
+	user := User{Name: "dj", Age: 18}
+	employee := Employee{}
+
+	copier.Copy(&employee, &user)
+	fmt.Printf("%#v\n", employee)
+	user.Name = "pop"
+	fmt.Println("user : ", user)
+	fmt.Println(employee)
+}
