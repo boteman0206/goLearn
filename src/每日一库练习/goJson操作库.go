84c5083af390060ad490fde1abbb4f44df9cf6c0
wei.peng <wei.peng@marcpoint.com>
2021-05-07 21:28:55 +0800

    合并结构体和map的库mergo

---
+++ src/每日一库练习/goJson操作库.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/thedevsaddam/gojsonq/v2"
+)
+
+func main() {
+	const json = `{"name":{"first":"Tom","last":"Hanks"},"age":61}`
+	name := gojsonq.New().FromString(json).Find("name.first")
+	fmt.Println(name.(string)) // Tom
+
+
+	const json1 = `{"city":"dhaka","type":"weekly","temperatures":[30,39.9,35.4,33.5,31.6,33.2,30.7]}`
+	avg := gojsonq.New().FromString(json1).From("temperatures").Avg()
+	fmt.Printf("Average temperature: %.2f", avg) // 33.471428571428575
+
+	fmt.Println("=================")
+
+
+
+}
