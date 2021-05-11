5989eab55c856939169ce13dc2d8588f37e48715
csf <410934912@qq.com>
2021-05-11 19:41:19 +0800

    测试删除密码阿达

--- src/test/多个参数.go
+++ src/test/多个参数.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/limitedlee/microservice/common/config"
+	"strings"
+)
 
 func Test(name ...string) {
 
@@ -11,5 +15,21 @@ func Test(name ...string) {
 }
 
 func main() {
+
 	Test("jack", "bob", "lucy")
+
+	name := "hello"
+	count := strings.Count(name, "") // todo 总的字节数
+	fmt.Println("count : ", count)
+
+	autho := "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjA4NzUwMjgsIm1vYmlsZSI6IjE4ODE4MjYxNDYxIiwibmFtZSI6InBlbmd3IiwidXNlcm5vIjoiVV8zRVk3QkNRIn0.SA-7iNS3kcNbmX7FvOIlhFEO_RJfhJzBOL8kE_yomUN3nbmkFFwpeUFhmz-JUFytq7tcdOMG27MX5NYGHUzZ6f-lFYPEEUhKnr5zmpePGSpPGNlJKL6BWQQsRN-v8QcAbupJL6B3vNaJwRu0Pbpc5nJ1HZNLqi5_BSSj7-qoELs"
+	index := strings.Index(autho, " ")
+	fmt.Println("index : ", index)
+
+	data := autho[strings.Index(autho, " ") : strings.Count(autho, "")-1]
+	fmt.Println("data : ", data)
+
+	//TODO 这个包是干嘛用的
+	get, err := config.Get("redis.Addr")
+	fmt.Println(err, " redis get : ", get)
 }
