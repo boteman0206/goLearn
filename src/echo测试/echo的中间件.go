627c3f53aa93db784c698d30c5637204262fdd78
pengwei <pengw@rp-pet.cn>
2021-05-18 12:13:05 +0800

    echo的中间件的级别和使用

---
+++ src/echo测试/echo的中间件.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+
+	e := echo.New()
+
+	// 注册路由之前的中间件
+	e.Pre(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
+		return func(context echo.Context) error {
+			fmt.Println("前置的路由级别设置。。。。")
+			return handlerFunc(context)
+		}
+	})
+
+	// 路由级别的中间件
+	e.Use(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fmt.Println("全局的中间件，所有的路由都会走到这里")
+			return handlerFunc(c)
+		}
+
+	})
+
+	group := e.Group("/my", func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
+		return func(context echo.Context) error {
+			fmt.Println("group 级别的中间件")
+			return handlerFunc(context)
+		}
+	})
+
+	group.GET("/get", get, func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
+		return func(context echo.Context) error {
+			fmt.Println("路由级别的中间件。。。。")
+			return handlerFunc(context)
+		}
+	})
+
+	e.Start(":9090")
+
+}
+
+func get(c echo.Context) error {
+	return c.JSON(http.StatusOK, "get 方法。。。")
+}
