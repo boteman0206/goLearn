f32653079337734c275ff78cba92b0cdf5713437
wei.peng <wei.peng@marcpoint.com>
2021-05-30 21:27:33 +0800

    反射三大定律

--- 反射/go反射/test1.go
+++ 反射/go反射/test1.go
@@ -26,19 +26,34 @@ func main() {
 		},
 	}
 
-	val := reflect.ValueOf(test)
+	val := reflect.ValueOf(&test)
 	typ := reflect.TypeOf(test)
 	/*
 		之前使用，test := &CoderTest{}指针传递出错 of.NumField()必须是指针
 		todo panic: reflect: call of reflect.Value.NumField on ptr Value
 
 	*/
-	field := val.NumField()
+	field := typ.NumField()
 
 	for i := 0; i < field; i++ {
-		fmt.Println("获取的欸一个key ： ", val.Field(i))
-		fmt.Println("typ 获取tag ： ", typ.Field(i).Tag.Get("tag"))
+		fmt.Println("获取的欸一个key ： ", val.Elem().Field(i))
+		get := typ.Field(i).Tag.Get("tag")
+		fmt.Println("typ 获取tag ： ", get)
+		s := val.Elem().Field(i).String()
+		if s == "laseName" && get == "myTag" {
+			fmt.Println("名称是laseName 并且 tag 是 myTag")
+			name := typ.Field(i).Name
+			byName := val.Elem().FieldByName(name)
+			fmt.Println("filed name is :", byName)
 
+			//todo 修改结构体内容
+			set := val.Elem().Field(i).CanSet()
+			if set {
+				val.Elem().Field(i).SetString("my change name")
+			}
+		}
 	}
+
+	fmt.Println("修改之后的name ： ", test)
 
 }
--- 反射/go反射/第一定律.go
+++ 反射/go反射/第一定律.go
@@ -21,8 +21,12 @@ func main() {
 
 	typ := reflect.TypeOf(coder)
 	val := reflect.ValueOf(&coder) // todo 使用val.Elem()，必须要是地址指针， 否则报错
+
 	fmt.Println("type: ", typ, " valuer: ", val, val.Elem())
 	fmt.Println("kind : ", typ.Kind(), typ.Field(0))
+
+	of := reflect.ValueOf(coder) // todo 注意 val.Field(0) 会报错， 因为上面的valueOf是指针
+	fmt.Println("of filed: ", of.Field(0))
 
 	stringElem := reflect.TypeOf((*fmt.Stringer)(nil)).Elem()
 	fmt.Println("stringElem: ", stringElem)
