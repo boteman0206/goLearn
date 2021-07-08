7971d675c3e1ab3d2a05d59f9e05ccfc473f9c6f
pengwei <pengw.rppet.cn>
2021-07-08 09:42:23 +0800

    疑难

---
+++ src/test/测试int的最大值.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	fmt.Println(math.MaxInt64)
+	fmt.Println(math.MaxInt8)
+	fmt.Println(math.MaxInt32)
+	fmt.Println(math.MaxUint32)
+
+}
