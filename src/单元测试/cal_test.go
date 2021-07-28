86e242a9fd189c8b06e0c14f6b7bb068d23f4573
wei.peng <wei.peng@marcpoint.com>
2021-07-28 21:31:29 +0800

    单元测试

--- src/单元测试/cal_test.go
+++ src/单元测试/cal_test.go
@@ -21,9 +21,50 @@ func TestCalYy(t *testing.T) {
 }
 
 func TestAddNum(t *testing.T) {
+	//t.SkipNow()  // todo 表示测试的时候跳过当前的方法不执行 执行 go test -v 的时候可以看到当前方法没有执行
 	AddNum(1, 10)
 	t.Logf("test add num ....")
 }
+
+/**
+	顺序指向的方法
+// 这个方式保证测试的顺序, 比如某个test测试依赖于另一个test的结果
+*/
+func TestShunxun(t *testing.T) {
+	t.Run("a1", func(t *testing.T) { fmt.Println("1") })
+	t.Run("a2", func(t *testing.T) { fmt.Println("2") })
+	t.Run("a3", func(t *testing.T) { fmt.Println("3") })
+}
+
+/**
+todo 如果要先执行某个test，则用 TestMain(m *testing.M) ，
+ 通常执行 文件打开 or 登录 or 数据库连接 等等
+*/
+
+func TestMain(m *testing.M) {
+	fmt.Println("test main first") // 会在文件的开始执行
+	m.Run()                        //不加 m.Run() 则不能进行后面的test
+}
+
+/**
+todo 测试性能是有 *test.B
+【BenchMark】
+ 把一个BenchMark重复很多次（不定，程序觉得跑到稳态了就算完，所以BenchMark里面最好是确定的执行时间，不然永远跑不到稳态，永远也跑不完），用来测试性能
+*/
+func BenchmarkAll(b *testing.B) { //函数名BenchmarkXXX，B大写，XXX首字母大写
+	for n := 0; n < b.N; n++ {
+		Print1to20()
+	}
+}
+func Print1to20() int {
+	res := 0
+	for i := 1; i <= 20; i++ {
+		res += i
+	}
+	return res
+}
+
+//todo 命令行执行 go test -bench=.     （程序只会跑带BenchMark的部分）
 
 /**
 
