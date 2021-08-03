50941b10c678670b37fdeadfcdf2c93b0906f705
pengwei <pengw.rppet.cn>
2021-08-03 11:57:02 +0800

    日志写入到redis

---
+++ src/每日一库练习/日志写到redis中.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	logredis "github.com/rogierlommers/logrus-redis-hook"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+)
+
+func init() {
+	hookConfig := logredis.HookConfig{
+		Host:     "localhost",
+		Key:      "mykey",
+		Format:   "v0",
+		App:      "syslog",
+		Hostname: "testapp",
+		Password: "demo",
+		Port:     6379,
+		DB:       0,
+		TTL:      3600,
+	}
+
+	hook, err := logredis.NewHook(hookConfig)
+	if err == nil {
+		logrus.AddHook(hook)
+	} else {
+		logrus.Errorf("logredis error: %q", err)
+	}
+}
+
+func main() {
+	logrus.Info("just some info logging...")
+
+	logrus.WithFields(logrus.Fields{
+		"animal": "walrus",
+		"foo":    "bar",
+		"this":   "that",
+	}).Info("additional fields are being logged as well")
+
+	logrus.SetOutput(ioutil.Discard)
+	logrus.Error("This will only be sent to Redis")
+}
