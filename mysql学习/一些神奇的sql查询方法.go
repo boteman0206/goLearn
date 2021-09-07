b4267e78743b35aab7351a0f204dd746e5dae8f0
pengwei <pengw.rppet.cn>
2021-09-07 21:18:38 +0800

    一些重要的方法

--- mysql学习/一些神奇的sql查询方法.go
+++ mysql学习/一些神奇的sql查询方法.go
@@ -37,5 +37,39 @@ package mysql学习
 				 GROUP BY P1.name
 				 ORDER BY rank_1;
 
+3：mysql的三值运算（三值运算图.jpg）
+    3.1： NOT IN 和 NOT EXISTS 不是等价的
+		（NOT IN 子查询中用到的表里被选择的列中存在 NULL，则 SQL 语句整体的查询结果永远是空。这是很可怕的现象。
+          为了得到正确的结果，我们需要使用 EXISTS 谓词。）
+		SELECT * FROM Class_A A
+			 WHERE NOT EXISTS ( SELECT *
+			 FROM Class_B B
+			 WHERE A.age = B.age
+			 AND B.city = '东京' );
+
+
+4： 限定谓词和极值函数不是等价的: ALL 谓词和ANY谓语和极值函数大多数时候都是一样的，但是在有null的情况下是不同的
+	原因：ALL 谓词其实是多个以 AND 连接的逻辑表达式的省略写法，所以有null的时候还是会返回不确定unknown
+         极值函数max, min等在统计时会把为 NULL 的数据排除掉
+
+
+5：按照现在的 SQL 标准来说，HAVING子句是可以单独使用的
+ 	5.1：having寻找缺失的编号
+	-- 如果有查询结果，说明存在缺失的编号
+		SELECT '存在缺失的编号' AS gap
+		 FROM SeqTbl
+		HAVING COUNT(*) <> MAX(seq);
+	 -- 查询缺失编号的最小值
+		SELECT MIN(seq + 1) AS gap
+		 FROM SeqTbl
+		 WHERE (seq+ 1) NOT IN ( SELECT seq FROM SeqTbl);
+	5.2： 用 HAVING 子句进行子查询 ：求众数（它指的是在群体中出现次数最多的值，）
+	 -- 求众数的 SQL 语句 (1)：使用谓词
+		SELECT income, COUNT(*) AS cnt
+		 FROM Graduates
+		 GROUP BY income
+		HAVING COUNT(*) >= ALL ( SELECT COUNT(*)
+		 FROM Graduates GROUP BY income);
+
 
 */
