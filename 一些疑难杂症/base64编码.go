836c53542dd0b2ae16709d0af3121836b255076f
pengwei <pengw.rppet.cn>
2021-09-14 09:44:20 +0800

    base64

--- 一些疑难杂症/base64编码.go
+++ 一些疑难杂症/base64编码.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"strings"
 )
 
 func main() {
@@ -22,12 +21,17 @@ func main() {
 	// todo 報錯 illegal base64 data at input byte 74
 	//todo 大致意思是，如果编码的时候字节不足会在最后加一到两个=号，但看我们的字符串最后没有=，解码的时候解到最后又报错了，赶紧给字符串手动加个等号试试
 
-	ciphertext = strings.Replace(ciphertext, " ", "", -1)
-	ciphertext = fmt.Sprintf(ciphertext, "==") // 有些需要有些不需要
+	//ciphertext = strings.Replace(ciphertext, " ", "", -1)
+	ciphertext = fmt.Sprint(ciphertext, "==") // 有些需要有些不需要
 	decoded1, err1 := base64.StdEncoding.DecodeString(ciphertext)
+	fmt.Println(string(decoded1), err1)
+
 	decoded2, err2 := base64.StdEncoding.DecodeString("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
 
-	fmt.Println(string(decoded1), err1)
 	fmt.Println(string(decoded2), err2)
 
+	// todo 解決方式二：直接使用RawURLEncoding 不用追加等号
+	encry := "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
+	bytes, err := base64.RawURLEncoding.DecodeString(encry)
+	fmt.Println(string(bytes), err)
 }
