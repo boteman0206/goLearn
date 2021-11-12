71f1b2015411ecaa62f0bdac9528c84519ceadbe
pengwei <pengw.rppet.cn>
2021-11-12 18:12:37 +0800

    repet

--- Repet常见问题/常见问题解决/订单相关.go
+++ Repet常见问题/常见问题解决/订单相关.go
@@ -1,8 +1,6 @@
 package 常见问题解决
 
-
-
-
+/**
 // 重新推送mq查询订单数据
 SELECT trim(both '"' from json_extract(content,'$.order_id')) as order_id
 from dc_order.Mq_info where quene='dc-sz-qqd-dispatch' and lastdate>='2021-11-12 00:00:00' ;
@@ -12,5 +10,4 @@ from dc_order.Mq_info where quene='dc-sz-qqd-dispatch' and lastdate>='2021-11-12
 SELECT trim(both '"' from json_extract(content,'$.order_id')) as order_id
 from dc_order.Mq_info where quene='dc-sz-qqd-dispatch' and lastdate>='2021-11-12 00:00:00' ;
 
-
-
+*/
