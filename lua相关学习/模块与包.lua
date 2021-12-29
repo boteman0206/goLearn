37f03561bfbfbeceeeb3612375fe42efeaf5aba6
wei.peng <wei.peng@marcpoint.com>
2021-12-29 22:55:56 +0800

    lua的模块使用

--- lua相关学习/模块与包.lua
+++ lua相关学习/模块与包.lua
@@ -3,3 +3,33 @@
 --- Created by Administrator.
 --- DateTime: 2021/12/29 22:24
 ---
+
+
+--[[
+    模块类似于一个封装库，从 Lua 5.1 开始，Lua 加入了标准的模块管理机制，可以把一些公用的代码放在一个文件里，以 API 接口的形式在其他地方调用，有利于代码的重用和降低代码耦合度。
+Lua 的模块是由变量、函数等已知元素组成的 table，因此创建一个模块很简单，就是创建一个 table，然后把需要导出的常量、函数放入其中，最后返回这个 table 就行。以下为创建自定义模块 module-baz.lua，文件代码格式如下：
+]]
+
+-- 定义一个名为module的模块
+module = {}
+
+-- 定义一个常量
+module.constant = "这是一个常量"
+
+
+-- 定义一个函数
+function module.func1()
+    return "this is func1 "
+end
+
+function module.func2()
+    print("这是一个私有函数")
+end
+
+function module.func3()
+    func2()
+end
+
+
+return module
+
