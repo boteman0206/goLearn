fd9ce14cd53635a3f1446e459fd9435440700bc1
pengwei <pengw.rppet.cn>
2022-04-11 12:15:35 +0800

    gorm的测试

--- src/gorm测试/gorm的时间格式.go
+++ src/gorm测试/gorm的时间格式.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"database/sql/driver"
+	"errors"
 	"fmt"
+	"github.com/spf13/cast"
+	"strings"
 	"time"
 )
 
@@ -85,4 +88,56 @@ func main() {
 
 	fmt.Println(jsonTime)
 
+	//myTime := MyTime(cast.ToTime(""))
+
+}
+
+//BaseModel 基础结构体 信息信息
+type BaseModel struct {
+	CreateTime MyTime `gorm:"comment:'创建时间';type:timestamp;";json:"createTime"`
+	UpdateTime MyTime `gorm:"comment:'修改时间';type:timestamp;";json:"updateTime"`
+	Remark     string `gorm:"comment:'备注'";json:"remark"`
+}
+
+//MyTime 自定义时间
+type MyTime time.Time
+
+func (t *MyTime) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+	var err error
+	//前端接收的时间字符串
+	str := string(data)
+	//去除接收的str收尾多余的"
+	timeStr := strings.Trim(str, "\"")
+	t1, err := time.Parse("2006-01-02 15:04:05", timeStr)
+	*t = MyTime(t1)
+	return err
+}
+
+func (t MyTime) MarshalJSON() ([]byte, error) {
+	formatted := fmt.Sprintf("\"%v\"", time.Time(t).Format("2006-01-02 15:04:05"))
+	return []byte(formatted), nil
+}
+
+func (t MyTime) Value() (driver.Value, error) {
+	// MyTime 转换成 time.Time 类型
+	tTime := time.Time(t)
+	return tTime.Format("2006-01-02 15:04:05"), nil
+}
+
+func (t *MyTime) Scan(v interface{}) error {
+	switch vt := v.(type) {
+	case time.Time:
+		// 字符串转成 time.Time 类型
+		*t = MyTime(vt)
+	default:
+		return errors.New("类型处理错误")
+	}
+	return nil
+}
+
+func (t *MyTime) String() string {
+	return fmt.Sprintf("hhh:%s", time.Time(*t).String())
 }
