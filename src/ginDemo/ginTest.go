1123a2a9b207cb0f4271f537af6c6aedd01332e8
pengwei <pengw.rppet.cn>
2022-06-28 09:38:57 +0800

    golang的事件驱动

--- src/ginDemo/ginTest.go
+++ src/ginDemo/ginTest.go
@@ -3,11 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/gookit/event"
 	"github.com/sirupsen/logrus"
+	kit "github.com/tricobbler/rp-kit"
 	"io"
 	"os"
 )
 
+//添加事件触发的操作
+
+func init() {
+	// Register event listener
+	event.On("evt1", event.ListenerFunc(func(e event.Event) error {
+		fmt.Printf("handle event: %s , %s, %s\n", e.Name(), "second", kit.JsonEncode(e.Data()))
+		return nil
+	}), event.Normal)
+
+	// Register multiple listeners
+	event.On("evt1", event.ListenerFunc(func(e event.Event) error {
+		fmt.Printf("handle event: %s, %s, %s\n", e.Name(), " first", kit.JsonEncode(e.Data()))
+		return nil
+	}), event.High)
+
+}
+
+// 添加日志打印功能
 func init() {
 
 	writer2 := os.Stdout
@@ -55,6 +75,13 @@ type Person struct {
 
 func getting(c *gin.Context) {
 	logrus.Info("geting runing ...")
+
+	// ... ...
+
+	// Trigger event
+	// Note: The second listener has a higher priority, so it will be executed first.
+	// get操作的时候触发
+	event.MustFire("evt1", event.M{"arg0": "val0", "arg1": "val1"})
 
 	c.JSON(200, "getting")
 }
