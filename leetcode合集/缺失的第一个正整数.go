f050414dc14b440bccfbce9a28ca497c9ecd4c3e
pengwei <pengw.rppet.cn>
2022-06-30 11:14:50 +0800

    缺失的正整数

--- leetcode合集/缺失的第一个正整数.go
+++ leetcode合集/缺失的第一个正整数.go
@@ -6,11 +6,24 @@ import (
 
 //给定一个未排序的整数数组nums，请你找出其中没有出现的最小的正整数
 
+/**
+思路使用map来存储数据，然后从1开始判断有没有存在的key
+*/
 func minNumberDisappeared(nums []int) int {
 	// write code here
 
-	if len(nums) == 1 {
-		return nums[0]
+	m := make(map[int]struct{}, len(nums))
+
+	for i := range nums {
+		m[nums[i]] = struct{}{}
+	}
+
+	var key = 1
+	for true {
+		if _, ok := m[key]; !ok {
+			return key
+		}
+		key++
 	}
 
 	return 0
@@ -18,7 +31,7 @@ func minNumberDisappeared(nums []int) int {
 
 func main() {
 
-	var numbers = []int{-1, 2, 3, 2, 2, 2, 5, 4, 2}
+	var numbers = []int{1, 2, 3, 2, 2, 2, 5, 4, 2}
 	solution := minNumberDisappeared(numbers)
 	fmt.Println(solution)
 }
