18a84a5f9a3056bcaa2a91d6ce8275681ff08b09
pengwei <pengw.rppet.cn>
2022-07-28 17:57:54 +0800

    mysql查询常用

--- leetcode合集/数组合并.go
+++ leetcode合集/数组合并.go
@@ -4,9 +4,15 @@ package main
 自己合并两个无序的数组为有序数组
 */
 
-//func SortIntS(num1, num2 []int) []int {
+//快速排序
+//func KuaiPaiSortIntS(num1, num2 []int) []int {
 //
 //}
+
+//冒泡排序
+func MaoPaoSortIntS(num1 []int) []int {
+
+}
 
 func main() {
 
---
+++ 一些疑难杂症/mysql一些常用的查询.go
@@ -0,0 +1,11 @@
+package main
+
+/**
+
+
+//查询正在执行的语句
+select * from information_schema.processlist WHERE info IS NOT NULL ORDER BY TIME desc;
+
+
+
+*/
