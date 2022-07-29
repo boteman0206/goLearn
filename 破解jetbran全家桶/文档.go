8c3a8d92d6f11ed6c032b18ea430ddd5ddf4897e
wei.peng <wei.peng@marcpoint.com>
2022-07-29 17:26:50 +0800

    破解jet方法

---
+++ 破解jetbran全家桶/文档.go
@@ -0,0 +1,18 @@
+package 破解jetbran全家桶
+
+/**
+
+
+1: 登录网址： https://jetbra.in/s
+
+2： 下载里面的 zip文件
+
+3： 解压下载的zip文件，将路径复制下来
+
+5： 找到下载的版本的vmoptions文件，可以使用Everything去搜索即可
+
+4： 将复制的路径添加到下载的goland的
+-javaagent:E:/chromeDownload/jetbra-bfcd6f85fcbc8859f7d096be09db50f2548f2eb8/jetbra/ja-netfilter.jar=jetbrains
+
+
+*/
