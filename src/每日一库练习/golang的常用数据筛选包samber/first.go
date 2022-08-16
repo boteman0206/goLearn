a1ad4cfc607d4a2cabe9c71eaa6727b3a2235c76
pengwei <pengw.rppet.cn>
2022-08-16 21:27:13 +0800

    samber/lo使用

--- src/每日一库练习/golang的常用数据筛选包samber/first.go
+++ src/每日一库练习/golang的常用数据筛选包samber/first.go
@@ -7,10 +7,12 @@ Lodash: 是一个javascript风格的库
 */
 
 import (
+	"errors"
 	"fmt"
 	"github.com/samber/lo"
 	"reflect"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -29,10 +31,81 @@ func main() {
 		//return cast.ToString(x)
 	})
 	fmt.Println(imap, reflect.TypeOf(imap))
-	var data string
-	for i := range imap {
-		data += imap[i]
-	}
-	fmt.Println(data)
 
+	find, b := lo.Find([]int{1, 2, 3, 4, 1}, func(x int) bool {
+		if x == 10 {
+			return true
+		}
+		return false
+	})
+	fmt.Println("Find: ", find, b)
+
+	of, i, b2 := lo.FindLastIndexOf([]int{1, 2, 3, 4, 1}, func(x int) bool {
+		if x == 1 {
+			return true
+		}
+		return false
+	})
+	fmt.Println("FindLastIndexOf: ", of, i, b2)
+
+	// 查找重复的数据
+	duplicates := lo.FindDuplicates([]int{1, 2, 3, 4, 1, 2, 3})
+	fmt.Println("FindDuplicates: ", duplicates)
+
+	min1 := lo.Min[int]([]int{1, 2, 3})
+	// 1
+	min2 := lo.Min[int]([]int{})
+	// 0
+	fmt.Println("min: ", min1, min2)
+
+	minBY := lo.MinBy[string]([]string{"s1", "string2", "s3"}, func(item string, min string) bool {
+		return len(item) < len(min)
+	})
+	// "s1"
+	fmt.Println("minBy:", minBY)
+
+	s := lo.Synchronize()
+	var num = 0
+	for i1 := 0; i1 < 10; i1++ {
+		go s.Do(func() {
+			num++
+		})
+	}
+	time.Sleep(2 * time.Second)
+	fmt.Println("Synchronize: ", num)
+
+	//方法包装
+	//Calls the function and return false in case of error and on panic. // 遇到panic或者err的时候返回false
+	ok := lo.Try(func() error {
+		//panic("error")
+		return errors.New("err")
+		//return nil
+	})
+
+	fmt.Println("try : ", ok)
+
+	//golang的TryCatch 和try的方法相同，但在发生错误时调用catch函数。
+	caught := false
+	lo.TryCatch(func() error {
+		panic("error")
+		return nil
+	}, func() {
+		caught = true
+	})
+	// false
+	// caught == true
+	fmt.Println("TryCatch: ", caught)
+
+	// 和try一样，但是会返回错误信息
+	err, ok := lo.TryWithErrorValue(func() error {
+		panic("error")
+		return nil
+	})
+	// "error", false
+	fmt.Println("TryWithErrorValue: ", err, ok)
+
+	in := []string{"", "foo", "", "bar", ""} // 返回非空的集合
+	slice := lo.Compact[string](in)
+	// []string{"foo", "bar"}
+	fmt.Println("Compact: ", slice)
 }
