891ae10fb235646814f52d016499ca3ae1b6d567
pengwei <pengw.rppet.cn>
2022-08-19 12:16:13 +0800

    等比例缩放

--- src/每日一库练习/图片压缩包/图片压缩处理包.go
+++ src/每日一库练习/图片压缩包/图片压缩处理包.go
@@ -7,12 +7,14 @@ import (
 
 func main() {
 
-	img, err := imaging.Open("D:\\RpPet\\gitProject\\goLearn\\src\\每日一库练习\\图片压缩包\\old.jpg")
+	img, err := imaging.Open("D:\\RpPet\\gitProject\\goLearn\\src\\每日一库练习\\图片压缩包\\430.jpg")
 	if err != nil {
 		fmt.Println("打开图片失败：", err.Error())
 	}
-	img = imaging.CropAnchor(img, 500, 800, imaging.Center) // 直接从中间裁剪，没有缩放
+	//img = imaging.CropAnchor(img, 500, 800, imaging.Center) // 直接从中间裁剪，没有缩放
 	//fmt.Println(src)
+
+	img = imaging.Resize(img, 800, 800, imaging.Lanczos) // 这个可以等比例缩放
 
 	err = imaging.Save(img, "D:\\RpPet\\gitProject\\goLearn\\src\\每日一库练习\\图片压缩包\\out_example.jpg")
 	fmt.Println(err)
