afc9cc147e0c60d9415cb68a457ec0f067ce037e
pengwei <pengw.rppet.cn>
2022-09-09 17:59:18 +0800

    激活jet

--- 破解jetbran全家桶/文档.go
+++ 破解jetbran全家桶/文档.go
@@ -11,8 +11,10 @@ package 破解jetbran全家桶
 
 5： 找到下载的版本的vmoptions文件，可以使用Everything去搜索即可
 
-4： 将复制的路径添加到下载的goland的
+4： 将复制的路径添加到下载的goland的 可能有问题
 -javaagent:E:/chromeDownload/jetbra-bfcd6f85fcbc8859f7d096be09db50f2548f2eb8/jetbra/ja-netfilter.jar=jetbrains
+
+上面的操作比较复杂可以直接执行里面的懒人脚本 在scripts目录下，点一下脚本就可以了
 
 
 */
