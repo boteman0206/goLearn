5a98618f75c0c08b0e6099ae76c89af2a0cb886d
pengwei <pengw.rppet.cn>
2022-09-29 15:20:08 +0800

    路由树实现

---
+++ 极客兔兔系列/web框架/gee/trie.go
@@ -0,0 +1,12 @@
+package gee
+
+/**
+gee的路由树实现
+*/
+
+type node struct {
+	pattern  string  // 待匹配路由，例如 /p/:lang
+	part     string  // 路由中的一部分，例如 :lang
+	children []*node // 子节点，例如 [doc, tutorial, intro]
+	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
+}
