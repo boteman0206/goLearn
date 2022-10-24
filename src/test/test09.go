513cc7f8853d09557e480d902323491eacdbddf0
pengwei <pengw.rppet.cn>
2022-10-24 09:46:09 +0800

    update

--- src/test/test09.go
+++ src/test/test09.go
@@ -1,24 +1,45 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	kit "github.com/tricobbler/rp-kit"
+	"sort"
+)
 
-type Obj struct {
-	mu sync.Mutex
-	// ... 其他字段
-}
-
-func (o *Obj) Lock()        { o.mu.Lock() }
-func (o *Obj) Dosomething() {}
-func (o *Obj) Unlock()      { o.mu.Unlock() }
+var (
+	format     = "15:04:05"
+	DateLayout = "2006-01-02 15:04:05"
+	Layout     = "2006-01-02"
+)
 
 func main() {
-	o := Obj{}
 
-	o.Lock()
-	o.Dosomething()
-	o.Unlock()
+	var ints = []int{1, 56, 2, 6567, 3, 5, 67}
 
-	o.Lock()
-	o.Dosomething()
-	o.Unlock()
+	fmt.Println("排序前：", ints)
+	sort.Slice(ints, func(i, j int) bool {
+		return ints[i] > ints[j]
+	})
+	fmt.Println("排序后: ", ints)
+
+	type TES01 struct {
+		Code int `json:"code"`
+		Sort int `json:"sort"`
+	}
+
+	var data []TES01
+	for i := 0; i < 10; i++ {
+		data = append(data, TES01{
+			Code: i,
+			Sort: 10 - i,
+		})
+	}
+	fmt.Println("排序前： ", kit.JsonEncode(data))
+
+	sort.Slice(data, func(i, j int) bool {
+
+		return data[i].Code > data[j].Code
+	})
+	fmt.Println("code排序：", kit.JsonEncode(data))
+
 }
