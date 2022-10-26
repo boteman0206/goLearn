7aee7943540a2480d9d912344ee4fccca470566b
pengwei <pengw.rppet.cn>
2022-10-26 10:17:29 +0800

    update

--- Repet常见问题/oms导入客户关系/导入库户关系sql.go
+++ Repet常见问题/oms导入客户关系/导入库户关系sql.go
@@ -8,4 +8,15 @@ package oms导入客户关系
 在拼接insert语句执行
 INSERT INTO dc_oms.base_customer_org_relation (org_id, is_default, create_by, create_time, customer_code)
 select id org_id, 0 is_default, 'admin' create_by , now() create_time,  'KH0004518' customer_code from dc_oms.base_organization bo where nc_code ='GYL024';
+
+
+
+
+导出对应的大仓的关系
+select bcor.customer_code "R1客户编码", bc.name "R1客户名称", bo.nc_code "nc编码", bo.name "对应大仓", bcor.org_id from dc_oms.base_customer_org_relation bcor
+join dc_oms.base_organization bo on bo.id = bcor.org_id
+join dc_oms.base_customer bc on bc.code = bcor.customer_code and bc.org_id = bcor.org_id
+where length(bo.nc_code) >0
+
+
 */
