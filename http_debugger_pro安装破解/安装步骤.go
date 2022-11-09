b6cc3e2c395f4a53f93a0dc6a7b37e0adcafba2b
pengwei <pengw.rppet.cn>
2022-11-09 11:07:59 +0800

    http debugger pro 的安装破解

---
+++ http_debugger_pro安装破解/安装步骤.go
@@ -0,0 +1,14 @@
+package http_debugger_pro安装破解
+
+/**
+1：将上面的文件解压，然后安装其中的软件 httpDebuggerPro
+
+2：解压之后里面还有一个破解软件 keygen
+
+3： 先解压，然后管理员运行，之后会弹出界面
+
+4、点击“Gen”——“Activate”即可（不需要复制注册码
+
+5、重新运行HTTP Debugger pro ，就破解成功了
+
+*/
