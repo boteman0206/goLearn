748d199893cb2e0bfce8f58fe8e2ce1d61fa6fc0
pengwei <pengw.rppet.cn>
2022-11-16 12:02:37 +0800

    redisbitmap

--- redis相关/图解redis系列/常用数据结构和命令.go
+++ redis相关/图解redis系列/常用数据结构和命令.go
@@ -149,14 +149,59 @@ GEORADIUS cars:locations 116.054579 39.030452 5 km ASC COUNT 10
 
 
 Stream
+Redis Stream 是 Redis 5.0 版本新增加的数据类型，Redis 专门为消息队列设计的数据类型。
+在 Redis 5.0 Stream 没出来之前，消息队列的实现方式都有着各自的缺陷，例如：
+	发布订阅模式，不能持久化也就无法可靠的保存消息，并且对于离线重连的客户端不能读取历史消息的缺陷；
+	List 实现消息队列的方式不能重复消费，一个消息消费完就会被删除，而且生产者需要自行实现全局唯一 ID。
+基于以上问题，Redis 5.0 便推出了 Stream 类型也是此版本最重要的功能，用于完美地实现消息队列，它支持消息的持久化、支持自动生成全局唯一 ID、支持 ack 确认消息的模式、支持消费组模式等，让消息队列更加的稳定和可靠。
+
+#常见命令
+XADD：插入消息，保证有序，可以自动生成全局唯一 ID； XADD mymq * name xiaolin   * 表示让 Redis 为插入的数据自动生成一个全局唯一的 ID
+XLEN ：查询消息长度；
+XREAD：用于读取消息，可以按 ID 读取数据；
+XDEL ： 根据消息 ID 删除消息；
+DEL ：删除整个 Stream；
+XRANGE ：读取区间消息
+XREADGROUP：按消费组形式读取消息；
+XPENDING 和 XACK：
+XPENDING 命令可以用来查询每个消费组内所有消费者「已读取、但尚未确认」的消息；
+XACK 命令用于向消息队列确认消息处理已完成；
+#应用场景
+消息队列
+	XADD mymq * name xiaolin
+		 * 表示让 Redis 为插入的数据自动生成一个全局唯一的 ID  1654254953808-0
+		往名称为 mymq 的消息队列中插入一条消息，消息的键是 name，值是 xiaolin
+	XREAD STREAMS mymq 1654254953807-0
+		从 ID 号为 1654254953807-0 的消息开始，读取后续的所有消息（示例中一共 1 条）。
+	XREAD BLOCK 10000 STREAMS mymq $
+		# 命令最后的“$”符号表示读取最新的消息
+		如果想要实现阻塞读（当没有数据时，阻塞住），可以调用 XRAED 时设定 BLOCK 配置项，实现类似于 BRPOP 的阻塞读取操作
+
+Stream 可以以使用 XGROUP 创建消费组，创建消费组之后，Stream 可以使用 XREADGROUP 命令让消费组内的消费者读取消息。
+XGROUP CREATE mymq group1 0-0    创建一个名为 group1 的消费组，0-0 表示从第一条消息开始读取。
+XGROUP CREATE mymq group2 0-0    创建一个名为 group2 的消费组，0-0 表示从第一条消息开始读取。
+XREADGROUP GROUP group1 consumer1 STREAMS mymq >   命令最后的参数“>”，表示从第一条尚未被消费的消息开始读取。
+		消息队列中的消息一旦被消费组里的一个消费者读取了，就不能再被该消费组内的其他消费者读取了，即同一个消费组里的消费者不能消费同一条消息。
+Streams 会自动使用内部队列（也称为 PENDING List）留存消费组里每个消费者读取的消息，直到消费者使用 XACK 命令通知 Streams“消息已经处理完成”。
+如果消费者没有成功处理消息，它就不会给 Streams 发送 XACK 命令，消息仍然会留存。此时，消费者可以在重启后，用 XPENDING 命令查看已读取、但尚未确认处理完成的消息。
+XPENDING mymq group2  我们来查看一下 group2 中各个消费者已读取、但尚未确认的消息个数
+XPENDING mymq group2 - + 10 consumer2   # 查看 group2 里 consumer2 已从 mymq 消息队列中读取了哪些消息
+XACK mymq group2 1654256265584-0   一旦消息 1654256265584-0 被 consumer2 处理了，consumer2 就可以使用 XACK 命令通知 Streams，然后这条消息就会被删除。
+
+小结：
+	消息保序：XADD/XREAD
+	阻塞读取：XREAD block
+	重复消息处理：Stream 在使用 XADD 命令，会自动生成全局唯一 ID；
+	消息可靠性：内部使用 PENDING List 自动保存消息，使用 XPENDING 命令查看消费组已经读取但是未被确认的消息，消费者使用 XACK 确认消息；
+	支持消费组形式消费数据
 
 
 
-
-
-
-
-
+Redis 基于 Stream 消息队列与专业的消息队列有哪些差距？
+一个专业的消息队列，必须要做到两大块：
+	1：消息不丢。
+	2：消息可堆积。
+Redis 本身可能会丢数据； 面对消息挤压，内存资源会紧张；
 
 
 */
