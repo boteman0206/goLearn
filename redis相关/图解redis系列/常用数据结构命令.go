35817e7edbb95a92dba4ea90beba57c2c5d52ad4
pengwei <pengw.rppet.cn>
2022-11-16 09:11:10 +0800

    redisbitmap

--- redis相关/图解redis系列/常用数据结构命令.go
+++ redis相关/图解redis系列/常用数据结构命令.go
@@ -109,10 +109,53 @@ Bitmap，即位图，是一串连续的二进制数组（0和1），可以通过
 SETBIT key offset value  # 设置值，其中value只能是 0 和 1
 GETBIT key offset   # 获取值
 BITCOUNT key start end  # 获取指定范围内值为 1 的个数  # start 和 end 以字节为单位
+BITPOS [key] [value] # 返回指定key中第一次出现指定value(0/1)的位置
 
 应用场景
 	Bitmap 类型非常适合二值状态统计的场景，这里的二值状态就是指集合元素的取值就只有 0 和 1 两种，在记录海量数据时，Bitmap 能够有效地节省内存空间。
 	签到统计
+	判断用户登陆态
+
+
+
+
+HyperLogLog
+简单来说 HyperLogLog 提供不精确的去重计数。
+PFADD key element [element ...]  # 添加指定元素到 HyperLogLog 中
+PFCOUNT key [key ...]  # 返回给定 HyperLogLog 的基数估算值。
+PFMERGE destkey sourcekey [sourcekey ...]   # 将多个 HyperLogLog 合并为一个 HyperLogLog
+
+百万级网页 UV 计数
+PFADD page1:uv user1 user2 user3 user4 user5
+这也就意味着，你使用 HyperLogLog 统计的 UV 是 100 万，但实际的 UV 可能是 101 万。虽然误差率不算大，但是，如果你需要精确统计结果的话，最好还是继续用 Set 或 Hash 类型。
+
+
+
+GEO
+Redis GEO 是 Redis 3.2 版本新增的数据类型，主要用于存储地理位置信息，并对存储的信息进行操作。
+GEO 本身并没有设计新的底层数据结构，而是直接使用了 Sorted Set 集合类型。
+GEOADD key longitude latitude member [longitude latitude member ...]   # 存储指定的地理空间位置，可以将一个或多个经度(longitude)、纬度(latitude)、位置名称(member)添加到指定的 key 中。
+GEOPOS key member [member ...]  # 从给定的 key 里返回所有指定名称(member)的位置（经度和纬度），不存在的返回 nil。
+GEODIST key member1 member2 [m|km|ft|mi]  # 返回两个给定位置之间的距离。
+GEORADIUS key longitude latitude radius m|km|ft|mi [WITHCOORD] [WITHDIST] [WITHHASH] [COUNT count] [ASC|DESC] [STORE key] [STOREDIST key]  # 根据用户给定的经纬度坐标来获取指定范围内的地理位置集合。
+
+
+应用场景：滴滴叫车
+执行下面的这个命令，就可以把 ID 号为 33 的车辆的当前经纬度位置存入 GEO 集合中
+GEOADD cars:locations 116.034579 39.030452 33
+例如，LBS 应用执行下面的命令时，Redis 会根据输入的用户的经纬度信息（116.054579，39.030452 ），查找以这个经纬度为中心的 5 公里内的车辆信息，并返回给 LBS 应用。
+GEORADIUS cars:locations 116.054579 39.030452 5 km ASC COUNT 10
+
+
+
+Stream
+
+
+
+
+
+
+
 
 
 
