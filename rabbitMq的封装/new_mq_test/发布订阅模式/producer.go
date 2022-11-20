0766e750574d997182f78834863cf3687e27b1dd
wei.peng <wei.peng@marcpoint.com>
2022-11-20 23:32:39 +0800

    mq的发布订阅

--- rabbitMq的封装/new_mq_test/发布订阅模式/producer.go
+++ rabbitMq的封装/new_mq_test/发布订阅模式/producer.go
@@ -10,6 +10,16 @@ import (
 /*
 http://rabbitmq.mr-ping.com/tutorials_with_golang/[3]Publish_Subscribe.html
 
+
+RabbitMq消息模式的核心思想是：一个生产者并不会直接往一个队列中发送消息，事实上，生产者根本不知道它发送的消息将被转发到哪些队列。
+实际上，生产者只能把消息发送给一个exchange，exchange只做一件简单的事情：一方面它们接收从生产者发送过来的消息，另一方面，它们把接收到的消息推送给队列。一个exchage必须清楚地知道如何处理一条消息。
+
+
+fanout总结
+1.生产者不是直接操作队列，而是将数据发送给交换机，由交换机将数据发送给与之绑定的队列。
+2：必须声明交换机，并且设置模式：channel.exchangeDeclare(EXCHANGE_NAME, "fanout")，
+	其中 fanout 指分发模式（将每一条消息都发送到与交换机绑定的队列）。
+3： 队列必须绑定交换机：channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
 */
 
 func Producer(url string, num int) {
