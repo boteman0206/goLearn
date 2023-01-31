8a2c10e9ccb6e4ea88f27905f0e56cbf212696a8
pengwei <pengwei@baidu.com>
2023-01-31 15:24:06 +0800

    不需要四舍五入

--- first.go
+++ first.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt" // 导入 fmt 包，打印字符串是需要用到
+	"github.com/shopspring/decimal"
 )
 
 func a() {
@@ -13,6 +14,9 @@ func main() { // 声明 main 主函数
 	fmt.Println("bc") // todo 同一行需要加分号 否则报错
 	a()
 	fmt.Println("this is a goodday")
+	price := 2.479446
+	priceRoundFloor, _ := decimal.NewFromFloat(price).RoundFloor(2).Float64()
+	fmt.Println(priceRoundFloor)
 
 }
 
