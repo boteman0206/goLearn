338e3b136866733e3991e422e18f1779843f7ee8
pengwei <pengwei@baidu.com>
2023-08-11 10:22:50 +0800

    k8s安装

--- k8s/main.go
+++ k8s/main.go
@@ -18,7 +18,6 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":3000", nil)
-}
 */
 
 func hello(w http.ResponseWriter, r *http.Request) {
