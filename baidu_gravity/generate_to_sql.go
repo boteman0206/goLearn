4db81b8c2418c736710ca8cf7bc55796c088780e
pengwei <pengwei@baidu.com>
2024-06-27 15:00:36 +0800

    生成sql

--- baidu_gravity/generate_to_sql.go
+++ baidu_gravity/generate_to_sql.go
@@ -12,24 +12,8 @@ import (
 **
  */
 
-// 1: 定义userID
-var userIDs = []int64{
-	1531008566, 1531018576, 1531028579, 1531052863, 1531063255, 1531092017,
-	1531092018, 1531093110, 1531093114, 1531093117, 1537581653, 1537583931, 1537584468,
-	1537584606, 1537584682, 1537585321, 1537585348, 1537585699, 1537585731, 1537585736,
-	1537585758, 1537585798, 1537585814, 1537585820, 1537585866, 1537585911, 1537585944,
-	1537585946, 1537585985, 1537585988, 1537585994, 1537586030, 1537586347, 1537586387,
-	1537586392, 1537586410, 1537586427, 1537586433, 1537586458, 1537586490,
-	1537586497, 1537586506, 1537586547, 1537586568, 1537586752,
-}
-
-// 2: 获取分表的规则
-func getTableID(userID int64) int64 {
-	return (userID % 10) + 1
-}
-
 // 3: 按照userID分表，分成组
-func getTableName() ([]int64, map[int64][]int64) {
+func getTableName(userIDs []int64) ([]int64, map[int64][]int64) {
 
 	mapv1 := make(map[int64]int64, 0)
 	mapv2 := make(map[int64][]int64, 0)
@@ -52,15 +36,10 @@ func getTableName() ([]int64, map[int64][]int64) {
 }
 
 // 4 生成sql
-func getTableScript() {
-	// 定义表名前缀和总表数量
-	tablePrefix := "tb_user_bind_"
-	// 定义查询模板
-	queryTemplate := "SELECT user_id, idfa FROM `%s%d` WHERE "
+func getTableScript(tablePrefix, queryTemplate string, tableids []int64, mapTabIdUserID map[int64][]int64) {
 	queries := make([]string, 0)
 
 	// 生成每个表的查询
-	tableids, mapTabIdUserID := getTableName()
 	var lenUid = 0
 	for i := range tableids {
 		uids := mapTabIdUserID[tableids[i]]
@@ -89,7 +68,28 @@ func getTableScript() {
 
 }
 
+// 1: 定义userID
+var ALLuserIDs = []int64{
+	1531008566, 1531018576, 1531028579, 1531052863, 1531063255, 1531092017,
+	1531092018, 1531093110, 1531093114, 1531093117, 1537581653, 1537583931, 1537584468,
+	1537584606, 1537584682, 1537585321, 1537585348, 1537585699, 1537585731, 1537585736,
+	1537585758, 1537585798, 1537585814, 1537585820, 1537585866, 1537585911, 1537585944,
+	1537585946, 1537585985, 1537585988, 1537585994, 1537586030, 1537586347, 1537586387,
+	1537586392, 1537586410, 1537586427, 1537586433, 1537586458, 1537586490,
+	1537586497, 1537586506, 1537586547, 1537586568, 1537586752,
+}
+
+// 2: 获取分表的规则 需要动态的调整
+func getTableID(userID int64) int64 {
+	return (userID % 10) + 1
+}
+
 func main() {
 
-	getTableScript()
+	tablePrefixId, tablegroupUserID := getTableName(ALLuserIDs)
+	// 定义表名前缀和总表数量
+	tablePrefixstr := "tb_user_bind_"
+	// 定义查询模板
+	queryTemplate := "SELECT user_id, idfa FROM `%s%d` WHERE "
+	getTableScript(tablePrefixstr, queryTemplate, tablePrefixId, tablegroupUserID)
 }
